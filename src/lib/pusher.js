dc23d0ea9b11a2810ebcb93feed0b5399f42aca0
Josh Nussbaum <joshnuss@gmail.com>
2021-09-24 18:01:36 -0400

    Adjust env vars

--- src/lib/pusher.js
+++ src/lib/pusher.js
@@ -4,5 +4,5 @@ export const pusher = new Pusher({
   appId: process.env['VITE_PUSHER_APP_ID'],
   key: process.env['VITE_PUSHER_KEY'],
   secret: process.env['VITE_PUSHER_SECRET'],
-  cluster: process.env['VITE_PUSHER_CLUSTER']
+  cluster: process.env['PUSHER_CLUSTER']
 })
