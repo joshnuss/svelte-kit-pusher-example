a8684b3d58544429039fdbeeb55fc0386887e164
Josh Nussbaum <joshnuss@gmail.com>
2021-09-24 18:23:23 -0400

    Fixup

--- src/lib/pusher.js
+++ src/lib/pusher.js
@@ -3,6 +3,6 @@ import Pusher from 'pusher'
 export const pusher = new Pusher({
   appId: process.env['VITE_PUSHER_APP_ID'],
   key: process.env['VITE_PUSHER_KEY'],
-  secret: process.env['VITE_PUSHER_SECRET'],
-  cluster: process.env['PUSHER_CLUSTER']
+  secret: process.env['PUSHER_SECRET'],
+  cluster: process.env['VITE_PUSHER_CLUSTER']
 })
